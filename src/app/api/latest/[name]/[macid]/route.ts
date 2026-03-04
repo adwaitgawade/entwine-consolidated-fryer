1d7fbe0a52f87a86137c572a5f4963fd33ae8f79
Adwait Gawade <adwaitg02@gmail.com>
2026-03-04 18:04:57 +0530

    feat: add API route to retrieve the latest file version by name and macid.

---
+++ src/app/api/latest/[name]/[macid]/route.ts
@@ -0,0 +1,22 @@
+import semver, { SemVer } from "semver";
+import type { NextRequest } from "next/server";
+import { getVersions, getObjectUrl } from "@/lib/aws";
+
+export async function GET(request: NextRequest, { params }: { params: Promise<{ name: string, macid: string }> }) {
+    const { name, macid } = await params;
+
+    const files = await getVersions(name + "/" + macid);
+
+    const versions = files.map((file: { version: string }) => file.version);
+    semver.sort(versions as unknown as SemVer[]).reverse();
+
+    const file: { key: string, version: string, url: string } | undefined = files.find((file: { version: string }) => file.version === versions[0]);
+
+    if (!file) {
+        return Response.json({ data: null, success: false, status: 404, error: "File not found" }, { status: 404 })
+    }
+
+    file.url = await getObjectUrl(file.key);
+
+    return Response.json({ data: file, success: true, status: 200, error: null }, { status: 200 });
+}
